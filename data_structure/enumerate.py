530abeb2225d500a93fcb0a096f07c24933cee02
Nusrat Jahan Shawon <nusrat.shawona27@gmail.com>
2025-07-23 18:54:39 +0600

    sorting intro

---
+++ data_structure/enumerate.py
@@ -0,0 +1,4 @@
+#enumerate() helps you loop through a list (or string, etc.) and gives you both the item and its position (index) at the same time.
+
+for index, char in enumerate("asdfghj"):
+    print(index, char)
