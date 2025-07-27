3006012a3a8ec92da1a5771267f5a7a8730b25d8
Nusrat Jahan Shawon <nusrat.shawona27@gmail.com>
2025-07-27 12:54:29 +0600

    update list_deleting with reversed way

--- data_structure/07.list_deleting.py
+++ data_structure/07.list_deleting.py
@@ -32,3 +32,24 @@ for index, value in enumerate(data):
 print(stop)
 del data[start:]
 print(data) 
+
+#--------------- deleting unsorted data backwords---------
+data = [104, 101, 4, 105, 308, 103, 5,
+        107, 100, 306, 106, 102, 108]
+min_valid = 100
+max_valid = 200
+
+for index in range(len(data) - 1, - 1, - 1):
+    if data[index] < min_valid or data[index] > max_valid:
+        print(index, data)
+        del data[index]
+
+print(data)
+print("--------------------------------")
+
+#-----------another way to do reversed deletion-----------
+top_index=len(data)-1
+for index, value in enumerate(reversed(data)):
+    if value <min_valid or value> max_valid:
+        print(top_index -index, value)
+    del data[top_index - index]
