cd8dd4dfbffa474f0a257f5d5a9f0e34706c5b5c
Nusrat Jahan Shawon <nusrat.shawona27@gmail.com>
2025-07-30 18:42:59 +0600

    more function  intro notes

--- functions/function_intro.py
+++ functions/function_intro.py
@@ -1,2 +1,29 @@
 # using functions is a great way to avoid duplicating code
 # also putting functions in code that allows us to use it over and over again in the code, without re-writing it
+
+# A function that is bound to an instance of a class is called method
+# def --- funtion defination start with the keyword 'def'
+# then next we give name of the funcction , here we used 'multiply'
+# () inside the perantheses we put our parameters. bt even if we dont have any perwameters we still will use the '( )'
+
+
+def multiply():
+    result = 10.5 * 4
+    return result
+
+
+answer=multiply()
+print(answer)
+# print(multiply())
+
+# by puting perameters
+# arguments are the values that will be used by formal parameters, when we call a function
+# we need to put values to providing an argument in the functiom
+# 
+def multiply(x,y):
+    result = x * y
+    return result
+
+
+answer=multiply(10, 3)
+print(answer)
