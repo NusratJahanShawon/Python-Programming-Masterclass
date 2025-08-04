da65a9a5f391c146fef427431adb18749f8766c3
Nusrat Jahan Shawon <nusrat.shawona27@gmail.com>
2025-08-04 14:12:14 +0600

    added docstring in banner_formating file

--- functions/banner_formatting_text.py
+++ functions/banner_formatting_text.py
@@ -1,5 +1,15 @@
 def banner_text(text: str="  ", screen_width:int=80) -> None: # here screem_width=80 is default parameter value
     # screen_width = 80
+    """ Print a string centred, with ** either side.
+  
+    :param text: The string to print.
+        An asterisk (*) will result in a row of asterisks.
+        The default will print a blank line, with a ** border at
+        the left and right edges.
+    :param screen_width: The overall width to print within
+        (including the 4 spaces for the ** either side).
+    :raises ValueError: if the supplied string is too long to fit.
+    """
     if len(text) > screen_width - 4:
         print("EEK!!")
         print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
